19db404f0e9cdd13308338bfad073b75a93e521d
pranavkalla16 <pranavkalla16@gmail.com>
2025-12-25 23:46:51 +0530

    Initial backend setup with Express

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
